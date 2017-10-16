b279f90b02fdeb2d0f1d854b0d5e7e8444b72797
Amine Ghilazghi <amineteclai@gmail.com>
2017-10-16 11:53:16 -0400

    Remove duplicate characters from input string

---
+++ StringManipulations/StringManipulations/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringManipulations")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringManipulations")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3e3ce8a5-ae85-4bac-93ef-1a0c47d75e74")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StringManipulations/StringManipulations/StringManipulation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringManipulations
+{
+    [TestClass]
+    public class StringManipulation
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var expected = "gole";
+            var actual = RemoveDuplicateChars("google");
+            Assert.AreEqual(expected, actual);
+        }
+
+        public string RemoveDuplicateChars(string input)
+        {
+            var result = string.Empty;
+
+            foreach (var item in input)
+            {
+                if (result.IndexOf(item) == -1) result += item;
+            }
+            return result;
+        }
+    }
+}
