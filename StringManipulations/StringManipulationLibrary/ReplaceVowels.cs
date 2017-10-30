67e59ea3f34cd40856c22d8515d4b375d305890a
Amine Ghilazghi <amineteclai@gmail.com>
2017-10-30 14:38:38 -0400

    Replaces vowels with $ sign

---
+++ StringManipulations/StringManipulationLibrary/ReplaceVowels.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulationLibrary
+{
+    public class ReplaceVowels
+    {
+        //Replace vowels in a string with $ sign
+        public string Replace(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return string.Empty;
+
+            var result = string.Empty;
+
+            foreach (var ch in input.ToLower())
+            {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                    result += "$";
+                else
+                    result += ch;
+            }
+
+            if (StartWithUpper(input))
+                return char.ToUpper(result[0]) + result.Substring(1);
+
+            return result;
+        }
+
+        public bool StartWithUpper(string word)
+        {
+            return Enumerable.Range(65, 26).Contains((int) word[0]);
+        }
+    }
+}
---
+++ StringManipulations/StringManipulations/ReplaceVowelsTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringManipulationLibrary;
+
+namespace StringManipulations
+{
+    [TestClass]
+    public class ReplaceVowelsTest
+    {
+        [TestMethod]
+        public void Test_Replace_InputStringWithVowel_ExpectedVowelsChangedWithDollarSign()
+        {
+            var vowel = new ReplaceVowels();
+            var expected = "H$ll$";
+            var actual = vowel.Replace("Hello");
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void Test_Replace_InputStringWithOutVowel_ExpectedReturnsTheSameLikeInput()
+        {
+            var vowel = new ReplaceVowels();
+            var expected = "Dry";
+            var actual = vowel.Replace("Dry");
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void Test_Replace_InputEmpytOrNull_ExpectedReturnsEmptyString()
+        {
+            var vowel = new ReplaceVowels();
+            string[] testNulls = {string.Empty, null};
+            var expected = string.Empty;
+
+            foreach (var item in testNulls)
+            {
+                var actual = vowel.Replace(item);
+                Assert.AreEqual(expected, actual);
+            }
+          
+        }
+    }
+}
