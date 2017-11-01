68bc1334c52401d76c9e5e5ac9b46afde459f8a9
Amine Ghilazghi <amineteclai@gmail.com>
2017-11-01 15:21:37 -0400

    Append similar character after original character

---
+++ StringManipulations/StringManipulationLibrary/RepeatChar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulationLibrary
+{
+    public class RepeatChar
+    {
+        /*
+         * Given a string, return a string where for every character in 
+         * the original append similar char
+         * For example: Repeat("The") --> TThhee
+         */
+        public string Repeat(string input)
+        {
+            var result = string.Empty;
+
+            for(var i = 0; i < input.Length; i++)
+            {
+                result += input.Substring(i, 1) + input.Substring(i, 1);
+            }
+            return result;
+        }
+    }
+}
---
+++ StringManipulations/StringManipulations/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringManipulationLibrary;
+
+namespace StringManipulations
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_Repeat_InputString_ExpectedRepeatedCharsReturnsRepeatedChars()
+        {
+            var chars = new RepeatChar();
+            var expected = "TThhee--##";
+            var actual = chars.Repeat("The-#");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
