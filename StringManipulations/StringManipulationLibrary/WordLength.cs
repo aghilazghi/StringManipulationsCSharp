eb5f11f6db8678201fdd3b93892401fdc7332f4b
Amine Ghilazghi <amineteclai@gmail.com>
2017-11-15 15:40:08 -0500

    Find a length of each word in a sentence

---
+++ StringManipulations/StringManipulationLibrary/WordLength.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulationLibrary
+{
+    public class WordLength
+    {
+        /*
+         * Find a length of each word in a sentence
+         */
+
+        public List<int> Count(string sentence)
+        {
+            var wordLength = sentence.Split().Select(w => w.Length).ToList();
+            return wordLength;
+        }
+    }
+}
---
+++ StringManipulations/StringManipulations/WordLengthTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringManipulationLibrary;
+using System.Collections.Generic;
+
+namespace StringManipulations
+{
+    [TestClass]
+    public class WordLengthTest
+    {
+        [TestMethod]
+        public void Count_Expected_4_2_1_4_8()
+        {
+            var wordLength = new WordLength();
+            var expectedNumbrs = new List<int>() { 4, 2, 1, 4, 8 };
+            List<int> actualNumbers = wordLength.Count("This is a long sentence");
+            CollectionAssert.AreEqual(expectedNumbrs, actualNumbers);
+        }
+    }
+}
