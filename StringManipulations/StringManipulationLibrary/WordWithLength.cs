30f73547af323e1f9baa25880d19c9970ae7ac79
Amine Ghilazghi <amineteclai@gmail.com>
2017-11-15 17:29:17 -0500

    Find a length of each word in a sentence and return the word and its length

---
+++ StringManipulations/StringManipulationLibrary/WordWithLength.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulationLibrary
+{
+    public class WordWithLength
+    {
+        /* Find a length of each word in a sentence and return 
+         * the word and its length
+         */
+        public List<WordLenMx> Count(string sentence)
+        {
+            if (string.IsNullOrWhiteSpace(sentence))
+                return Enumerable.Empty<WordLenMx>().ToList();
+
+            List<WordLenMx> wordsWithLength = sentence.Split()
+                                    .Select(w => new WordLenMx
+                                    {
+                                        Word = w,
+                                        Count = w.Length
+                                    }).ToList();
+
+            return wordsWithLength;
+        }
+    }
+
+    public class WordLenMx
+    {
+        public string Word { get; set; }
+        public int Count { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            WordLenMx other = obj as WordLenMx;
+            if (other == null)
+            {
+                return false;
+            }
+            return (this.Word == other.Word) && (this.Count == other.Count);
+        }
+
+        public override int GetHashCode()
+        {
+            return 33 * Word.GetHashCode() + Count.GetHashCode();
+        }
+    }
+}
---
+++ StringManipulations/StringManipulations/WordWithLengthTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringManipulationLibrary;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringManipulations
+{
+    [TestClass]
+    public class WordWithLengthTest
+    {
+        [TestMethod]
+        public void Count_Expected_This_is_a_sentence()
+        {
+            var wordWithLen = new WordWithLength();
+            var expected = new List<WordLenMx>()
+            {
+                new WordLenMx { Word = "This", Count = 4},
+                new WordLenMx { Word = "is", Count = 2},
+                new WordLenMx { Word = "a", Count = 1},
+                new WordLenMx { Word = "sentence", Count = 8}
+            };
+
+            var actual = wordWithLen.Count("This is a sentence");
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Count_Expected_Empty_List_With_Empty_String_Input()
+        {
+            var wordWithLen = new WordWithLength();
+            var expected = Enumerable.Empty<WordLenMx>().ToList();
+            var actual = wordWithLen.Count(" ");
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Count_Expected_Empty_List_With_Null_Input()
+        {
+            var wordWithLen = new WordWithLength();
+            var expected = Enumerable.Empty<WordLenMx>().ToList();
+            var actual = wordWithLen.Count(null);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
